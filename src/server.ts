a048f9758221e2e05b2175e21f5a4e2470545794
abdulvahabaa <abdulvahabaa916@gmail.com>
2025-11-05 13:53:44 +0530

    feat:cors and helmet implimented

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 import connectDB from './config/db.js';
 import userRoutes from './routes/user.routes.js';
 import { errorHandler } from './middleware/error.middleware.js';
@@ -8,7 +10,14 @@ import { errorHandler } from './middleware/error.middleware.js';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+
+// Middleware
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
@@ -16,16 +25,15 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('combined')); // Better log format for production
 }
 
+await connectDB();
+
 // Routes
 app.use('/api/users', userRoutes);
 
 // Error Middleware
 app.use(errorHandler);
 
-// Start server after DB connects
-const PORT = process.env.PORT || 5000;
-
-await connectDB();
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT} in ${process.env.NODE_ENV} mode`),
