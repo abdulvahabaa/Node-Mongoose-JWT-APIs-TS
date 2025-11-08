f07376ad7548083b6e8446e039b319703182296c
abdulvahabaa <abdulvahabaa916@gmail.com>
2025-11-08 20:42:54 +0530

    feat:logout with blacklisting token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { loginUser, registerUser } from '../controllers/auth.controller.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/auth.controller.js';
 
 const authRoutes = Router({ mergeParams: true });
 
@@ -8,5 +8,8 @@ authRoutes.post('/register', registerUser);
 
 // POST /api/auth/login
 authRoutes.post('/login', loginUser);
+
+// POST /api/auth/logout
+authRoutes.post('/logout', logoutUser);
 
 export default authRoutes;
